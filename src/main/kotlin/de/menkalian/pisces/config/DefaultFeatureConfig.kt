aa60808695f66f291bda500c02ebaef4bee2f155
Kilian Krampf <kilian@krampf.de>
2021-08-21 19:06:56 +0200

    `default`-Deklarationen der Features angepasst

--- src/main/kotlin/de/menkalian/pisces/config/DefaultFeatureConfig.kt
+++ src/main/kotlin/de/menkalian/pisces/config/DefaultFeatureConfig.kt
@@ -6,23 +6,18 @@ package de.menkalian.pisces.config
 class DefaultFeatureConfig : FeatureConfig() {
     override fun configure() {
         pisces.audio.apply {
-            Handler.enable()
         }
 
         pisces.command.apply {
-            Handler.enable()
         }
 
         pisces.database.apply {
-            Handler.enable()
         }
 
         pisces.discord.apply {
-            Handler.enable()
         }
 
         pisces.message.apply {
-            Handler.enable()
         }
     }
 }
