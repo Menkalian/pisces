d1b6c36dac319d42f99ee4de7dbdbed83e927150
Kilian Krampf <kilian@krampf.de>
2021-09-09 23:50:41 +0200

    IAudioHandler Schnittstelle definiert

--- src/main/kotlin/de/menkalian/pisces/audio/IAudioHandler.kt
+++ src/main/kotlin/de/menkalian/pisces/audio/IAudioHandler.kt
@@ -2,4 +2,28 @@ package de.menkalian.pisces.audio
 
 import de.menkalian.pisces.IHandler
 
-interface IAudioHandler : IHandler
+/**
+ * Schnittstelle zur Bearbeitung von Audio.
+ * Eine Instanz dieser Schnittstelle wird als Spring-Bean in der Applikation zur Verfügung gestellt.
+ * Der AudioHandler verwaltet die aktiven [IGuildAudioController]-Instanzen und stellt diese (threadsicher) zur Verfügung
+ */
+interface IAudioHandler : IHandler {
+    /**
+     * Stellt den [IGuildAudioController] zur angegebenen ID bereit.
+     * Falls zum geforderten Discord-Server kein Controller verfügbar ist, wird eine neue Instanz erzeugt.
+     *
+     * @param guildId Discord-ID des Servers
+     *
+     * @return [IGuildAudioController] des Servers
+     */
+    fun getGuildAudioController(guildId: Long): IGuildAudioController
+
+    /**
+     * Löscht/Entfernt den [IGuildAudioController] des angegebenen Servers.
+     *
+     * @param guildId Discord-ID des Servers
+     *
+     * @return Ob ein [IGuildAudioController] gelöscht wurde (z.B. `false` wenn kein [IGuildAudioController] für die angegebene ID existiert)
+     */
+    fun deleteGuildAudioController(guildId: Long): Boolean
+}
