a2d7171bc040cf94d6eacd3a11a2d8422a92bb3f
Kilian Krampf <kilian@krampf.de>
2021-09-12 21:23:38 +0200

    Kommandoschnittstelle definiert

--- src/main/kotlin/de/menkalian/pisces/command/ICommand.kt
+++ src/main/kotlin/de/menkalian/pisces/command/ICommand.kt
@@ -1,13 +1,17 @@
 package de.menkalian.pisces.command
 
+import de.menkalian.pisces.util.FixedVariables
 import net.dv8tion.jda.api.entities.ChannelType
 
+/**
+ * Schnittstelle eines Befehls.
+ * Befehle sind die Schnittstelle zwischen dem Benutzer und den Bots.
+ *
+ * Über Dependency-Injection werden automatisch alle Kommandos, die als Spring-Bean vorliegen
+ */
 interface ICommand {
     enum class ECommandChannelContext {
-        PRIVATE,
-        GUILD_ALL,
-        GUILD_TEXT,
-        GUILD_STORE;
+        PRIVATE, GUILD_ALL, GUILD_TEXT, GUILD_STORE;
 
         fun supports(type: ChannelType) =
             type.isMessage && when (this) {
@@ -18,9 +22,29 @@ interface ICommand {
             }
     }
 
+    enum class ECommandSource { TEXT, COMMAND }
+    enum class ParameterType { INTEGER, STRING, USER, DATE, TIME }
+
+    data class CommandParameter(
+        val name: String, val short: Char,
+        val description: String,
+        val type: ParameterType,
+        val defaultValue: Any, val currentValue: Any = defaultValue
+    )
+
+    val name: String
+    val aliases: List<String>
+    val description: String
+    val argumentParameter: CommandParameter
+    val parameters: List<CommandParameter>
+
     val supportedContexts: List<ECommandChannelContext>
     infix fun supports(type: ChannelType) =
         supportedContexts.any { it.supports(type) }
 
+    val supportedSources: List<ECommandSource>
+    infix fun supports(source: ECommandSource) =
+        supportedSources.contains(source)
 
+    fun execute(source: ECommandSource, parameters: List<CommandParameter>, sourceInformation: FixedVariables)
 }
--- src/main/kotlin/de/menkalian/pisces/command/ICommandHandler.kt
+++ src/main/kotlin/de/menkalian/pisces/command/ICommandHandler.kt
@@ -2,4 +2,12 @@ package de.menkalian.pisces.command
 
 import de.menkalian.pisces.IHandler
 
-interface ICommandHandler : IHandler
+/**
+ * Der [ICommandHandler] verwaltet die Commands
+ */
+interface ICommandHandler : IHandler {
+    fun execute(cmdString: String)
+    fun execute(name: String, parameters: HashMap<String, String>)
+
+    val commandList: List<ICommand>
+}
