e979b42203f6d39ba2eff9ae5106ca6b1bb0d9b3
Kilian Krampf <kilian@krampf.de>
2021-09-12 21:20:39 +0200

    Variablen deklariert

--- src/main/kotlin/de/menkalian/pisces/util/Datatypes.kt
+++ src/main/kotlin/de/menkalian/pisces/util/Datatypes.kt
@@ -7,3 +7,6 @@ import de.menkalian.pisces.audio.data.TrackInfo
  * Kombination aus dem Lade/Suchergebnis eines Tracks und den Informationen zu dem/den geladenen Track(s).
  */
 typealias QueueResult = Pair<EPlayTrackResult, List<TrackInfo>>
+
+typealias Variables = MutableMap<String, String>
+typealias FixedVariables = Map<String, String>
