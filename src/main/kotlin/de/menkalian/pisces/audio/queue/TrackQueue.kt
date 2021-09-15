9d5479a8101433214d9398d3635bf475534a613c
Menkalian <kilian@krampf.de>
2021-09-16 00:07:59 +0200

    TrackQueue erweitert

--- src/main/kotlin/de/menkalian/pisces/audio/queue/TrackQueue.kt
+++ src/main/kotlin/de/menkalian/pisces/audio/queue/TrackQueue.kt
@@ -1,7 +1,17 @@
 package de.menkalian.pisces.audio.queue
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import de.menkalian.pisces.util.logger
 
 class TrackQueue : MutableList<AudioTrack> by mutableListOf() {
-    fun nextTrack(isShuffle: Boolean) = if (isShuffle) random() else first()
+    fun nextTrack(isShuffle: Boolean): AudioTrack? {
+        if(isEmpty()) {
+            logger().info("Reached end of queue $this")
+            return null
+        }
+
+        val nextTrack = if (isShuffle) random() else first()
+        logger().debug("Providing next track: $nextTrack.ma.")
+        return nextTrack
+    }
 }
