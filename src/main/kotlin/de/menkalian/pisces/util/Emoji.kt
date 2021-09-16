b45c0d96b5d6c25ee70ff09144474bf4598ba7f9
Menkalian <kilian@krampf.de>
2021-09-16 23:29:59 +0200

    Emoji hinzugefügt

--- src/main/kotlin/de/menkalian/pisces/util/Emoji.kt
+++ src/main/kotlin/de/menkalian/pisces/util/Emoji.kt
@@ -6,6 +6,9 @@ package de.menkalian.pisces.util
  * Enthält Konstanten für die Emoji-Namen von Discord.
  * Die Emojis können in Discord nachgeschaut werden, daher wird keine KDoc-Beschreibung für alle Felder angelegt.
  */
-object Emoji {
+internal object Emoji {
     const val SLIGHT_SMILE = ":slight_smile:"
+
+    const val ANIMAL_WHALE = ":whale:"
+    const val ANIMAL_WHALE2 = ":whale2:"
 }
