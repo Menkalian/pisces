80a94a83f265d15d1db4852782fad727b8e5a3a1
Menkalian <kilian@krampf.de>
2021-09-19 02:34:06 +0200

    Emoji als Unicode-Zeichen definiert

--- src/main/kotlin/de/menkalian/pisces/util/Emoji.kt
+++ src/main/kotlin/de/menkalian/pisces/util/Emoji.kt
@@ -7,8 +7,11 @@ package de.menkalian.pisces.util
  * Die Emojis können in Discord nachgeschaut werden, daher wird keine KDoc-Beschreibung für alle Felder angelegt.
  */
 internal object Emoji {
-    const val SLIGHT_SMILE = ":slight_smile:"
+    const val SLIGHT_SMILE = "\uD83D\uDE42"
 
-    const val ANIMAL_WHALE = ":whale:"
-    const val ANIMAL_WHALE2 = ":whale2:"
+    const val ARROW_DOWN = "\u2B07\uFE0F"
+    const val ARROW_UP = "\u2B06\uFE0F"
+
+    const val ANIMAL_WHALE = "\uD83D\uDC33"
+    const val ANIMAL_WHALE2 = "\uD83D\uDC0B"
 }
