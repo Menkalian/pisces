2eec157e4fd32593307398e20342359b6f6cf644
Menkalian <kilian@krampf.de>
2021-09-19 02:34:52 +0200

    Formatmethoden erweitert

--- src/main/kotlin/de/menkalian/pisces/util/FormatUtils.kt
+++ src/main/kotlin/de/menkalian/pisces/util/FormatUtils.kt
@@ -1,3 +1,7 @@
 package de.menkalian.pisces.util
 
 fun String.asCodeBlock() = "```$this```"
+fun String.asBold() = "**$this**"
+fun String.asItalic() = "*$this*"
+fun String.asSpoiler() = "||$this||"
+fun String.asInlineCode() = "`$this`"
