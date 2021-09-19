c23863771726497ad1a7d6ade189dc1cf1979a36
Menkalian <kilian@krampf.de>
2021-09-19 02:31:48 +0200

    Standardfarbe für Pisces definiert

--- src/main/kotlin/de/menkalian/pisces/util/Datatypes.kt
+++ src/main/kotlin/de/menkalian/pisces/util/Datatypes.kt
@@ -2,6 +2,8 @@ package de.menkalian.pisces.util
 
 import de.menkalian.pisces.audio.data.EPlayTrackResult
 import de.menkalian.pisces.audio.data.TrackInfo
+import java.awt.Color
+import java.nio.ByteBuffer
 
 /**
  * Kombination aus dem Lade/Suchergebnis eines Tracks und den Informationen zu dem/den geladenen Track(s).
@@ -10,3 +12,20 @@ typealias QueueResult = Pair<EPlayTrackResult, List<TrackInfo>>
 
 typealias Variables = MutableMap<String, String>
 typealias FixedVariables = Map<String, String>
+
+object PiscesColor {
+    val red: UByte = 121u
+    val green: UByte = 170u
+    val blue: UByte = 247u
+
+    val colorInt: Int
+        get() = ByteBuffer.allocate(4)
+            .put(0x1F)
+            .put(red.toByte())
+            .put(green.toByte())
+            .put(blue.toByte())
+            .getInt(0)
+
+    val colorAwt: Color
+        get() = Color(colorInt)
+}
