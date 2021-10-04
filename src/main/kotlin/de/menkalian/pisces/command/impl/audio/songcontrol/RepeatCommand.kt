598361e93e9e6865071b693b51d4d68f22c63daf
Kilian Krampf <kilian@krampf.de>
2021-10-04 23:32:29 +0200

    Pause/PShuffle/Repeat-Befehle implementiert

---
+++ src/main/kotlin/de/menkalian/pisces/command/impl/audio/songcontrol/RepeatCommand.kt
@@ -0,0 +1,59 @@
+package de.menkalian.pisces.command.impl.audio.songcontrol
+
+import de.menkalian.pisces.OnConfigValueCondition
+import de.menkalian.pisces.RequiresKey
+import de.menkalian.pisces.audio.IAudioHandler
+import de.menkalian.pisces.command.CommonCommandBase
+import de.menkalian.pisces.command.ICommandHandler
+import de.menkalian.pisces.command.data.CommandParameter
+import de.menkalian.pisces.command.data.ECommandSource
+import de.menkalian.pisces.database.IDatabaseHandler
+import de.menkalian.pisces.message.IMessageHandler
+import de.menkalian.pisces.util.FixedVariables
+import org.springframework.context.annotation.Conditional
+import org.springframework.stereotype.Component
+
+@Component
+@Conditional(OnConfigValueCondition::class)
+@RequiresKey(["pisces.command.impl.audio.Repeat"])
+class RepeatCommand(
+    override val databaseHandler: IDatabaseHandler,
+    val messageHandler: IMessageHandler,
+    val audioHandler: IAudioHandler
+) : CommonCommandBase() {
+    override fun initialize() {
+        aliases.add("wiederhole")
+
+        supportedContexts.addAll(ALL_GUILD_CONTEXTS)
+        supportedSources.addAll(ALL_SOURCES)
+
+        super.initialize()
+    }
+
+    override val name: String
+        get() = "repeat"
+    override val description: String
+        get() = "Wechselt den aktuellen Wiederholungs-Status"
+
+    override fun execute(
+        commandHandler: ICommandHandler,
+        source: ECommandSource,
+        parameters: List<CommandParameter>,
+        guildId: Long,
+        channelId: Long,
+        authorId: Long,
+        sourceInformation: FixedVariables
+    ) {
+        val controller = audioHandler.getGuildAudioController(guildId)
+
+        messageHandler
+            .createMessage(guildId, channelId)
+            .withTitle(
+                if (controller.togglePause())
+                    "Wiederholung wurde aktiviert"
+                else
+                    "Wiederholung wurde deaktiviert"
+            )
+            .build()
+    }
+}
