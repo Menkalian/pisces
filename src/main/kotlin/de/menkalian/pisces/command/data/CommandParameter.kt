63f645359a8f0d667d359407d311b407bb0d94fe
Kilian Krampf <kilian@krampf.de>
2021-10-07 19:45:49 +0200

    Dokumentation/Logging erweitert

--- src/main/kotlin/de/menkalian/pisces/command/data/CommandParameter.kt
+++ src/main/kotlin/de/menkalian/pisces/command/data/CommandParameter.kt
@@ -4,17 +4,96 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
+/**
+ * Datenstruktur für einen **optionalen** zusätzlichen Aufrufparameter.
+ * Jedes [ICommand][de.menkalian.pisces.command.ICommand]-Objekt kann beliebig viele Parameter erhalten (auch 0).
+ *
+ * @property name Name des Parameters. Kann durch das Voranstellen von `--` übergeben werden (z.B. `--name pisces`)
+ * @property short Buchstabe als kurze Version des Parameters. Kann durch das Voranstellen von `-` übergeben werden (z.B. `-n pisces`)
+ * @property description Beschreibung der Bedeutung/Funktion des Parameters
+ * @property type Typ des Parameters
+ * @property defaultValue Standardwert des Parameters.
+ *                        Der tatsächliche Datentyp hängt von [type] ab und ist in der Dokumentation von [EParameterType] vermerkt.
+ *                        Hier **muss** der korrekte Datentyp verwendet werden, sonst werden können unerwartete Werte als Standardwerte geliefert werden.
+ *                        Es kommt jedoch **nicht** zu Fehlern/Exceptions.
+ * @property currentValue Aktueller tatsächlicher Wert des Parameters. Für den Datentyp gilt das gleiche wie für [defaultValue].
+ */
 data class CommandParameter(
     val name: String, val short: Char,
     val description: String,
     val type: EParameterType,
     val defaultValue: Any, var currentValue: Any = defaultValue
 ) {
-    fun asBoolean() = currentValue.toString().toBooleanStrictOrNull() ?: defaultValue.toString().toBoolean()
-    fun asInt() = currentValue.toString().toIntOrNull() ?: defaultValue.toString().toInt()
+    /**
+     * Interpretiert den aktuellen Wert als [Boolean] und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - `false`
+     *
+     * @return Aktueller Wert als Wahrheitswert.
+     */
+    fun asBoolean() = currentValue.toString().toBooleanStrictOrNull() ?: defaultValue.toString().toBooleanStrictOrNull() ?: false
+
+    /**
+     * Interpretiert den aktuellen Wert als [Int] und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - `-1`
+     *
+     * @return Aktueller Wert als Ganzzahl.
+     */
+    fun asInt() = currentValue.toString().toIntOrNull() ?: defaultValue.toString().toIntOrNull() ?: -1
+
+    /**
+     * Interpretiert den aktuellen Wert als [String] und gibt ihn zurück.
+     *
+     * @return Aktueller Wert als Zeichenkette.
+     */
     fun asString() = currentValue.toString()
-    fun asUserId() = currentValue.toString().toLongOrNull() ?: -1L
+
+    /**
+     * Interpretiert den aktuellen Wert als [Long] (UserID) und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - `-1L`
+     *
+     * @return Aktueller Wert als Long ID.
+     */
+    fun asUserId() = currentValue.toString().toLongOrNull() ?: defaultValue.toString().toLongOrNull() ?: -1L
+
+    /**
+     * Interpretiert den aktuellen Wert als [LocalDateTime] und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - [LocalDateTime.MIN] (not null)
+     *
+     * @return Aktueller Wert als Zeitpunkt.
+     */
     fun asTimestamp() = currentValue as? LocalDateTime ?: defaultValue as? LocalDateTime ?: LocalDateTime.MIN!!
+
+    /**
+     * Interpretiert den aktuellen Wert als [LocalDate] und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - [LocalDate.MIN] (not null)
+     *
+     * @return Aktueller Wert als Datum.
+     */
     fun asDate() = currentValue as? LocalDate ?: defaultValue as? LocalDate ?: LocalDate.MIN!!
+
+    /**
+     * Interpretiert den aktuellen Wert als [LocalTime] und gibt ihn zurück.
+     * Falls es Probleme gibt einen Wert zu interpretieren, wird die folgende Reihenfolge verwendet:
+     *  - [currentValue] falls gültig, sonst
+     *  - [defaultValue] falls gültig, sonst
+     *  - [LocalTime.MIN] (not null)
+     *
+     * @return Aktueller Wert als Uhrzeit.
+     */
     fun asTime() = currentValue as? LocalTime ?: defaultValue as? LocalTime ?: LocalTime.MIN!!
 }
--- src/main/kotlin/de/menkalian/pisces/command/listener/SlashCommandListener.kt
+++ src/main/kotlin/de/menkalian/pisces/command/listener/SlashCommandListener.kt
@@ -1,23 +1,21 @@
 package de.menkalian.pisces.command.listener
 
-import de.menkalian.pisces.IHandler
 import de.menkalian.pisces.OnConfigValueCondition
 import de.menkalian.pisces.RequiresKey
 import de.menkalian.pisces.command.ICommand
 import de.menkalian.pisces.command.ICommandHandler
 import de.menkalian.pisces.command.data.ECommandSource
 import de.menkalian.pisces.command.data.EParameterType
-import de.menkalian.pisces.discord.IDiscordHandler
 import de.menkalian.pisces.util.Variables
 import de.menkalian.pisces.util.logger
 import de.menkalian.pisces.util.shortenTo
 import de.menkalian.pisces.variables.FlunderKey
 import net.dv8tion.jda.api.entities.ChannelType
+import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
-import org.springframework.beans.factory.BeanFactory
 import org.springframework.context.annotation.Conditional
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -26,66 +24,65 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 
+/**
+ * Listener zuständig für die Verarbeitung der sogenannten "SlashCommands" von Discord.
+ * Dieser Listener registriert die gültigen Commands bei Discord, verarbeitet diese bei Ausführung und parst die übergebenen Parameter.
+ */
 @Suppress("RedundantModalityModifier")
 @Service
 @Conditional(OnConfigValueCondition::class)
 @RequiresKey(["pisces.command.SlashCommandListener"])
-class SlashCommandListener(
-    val beanFactory: BeanFactory,
-    final val commandHandler: ICommandHandler
-) : ListenerAdapter(), IHandler.IInitializationHandler {
+class SlashCommandListener(final val commandHandler: ICommandHandler) : ListenerAdapter() {
     private val commandMap: MutableMap<Long, ICommand> = hashMapOf()
 
-    init {
-        commandHandler.addInitializationHandler(this)
-    }
+    override fun onReady(event: ReadyEvent) {
+        val jda = event.jda
+        val updateAction = jda.updateCommands()
+        val comNameMap = commandHandler.commandsByName
+        logger().info("Initializing native Discord commands")
 
-    override fun onInitialized(handler: IHandler) {
-        if (handler !is IDiscordHandler) {
-            beanFactory.getBean(IDiscordHandler::class.java).addInitializationHandler(this)
-            return
-        } else {
-            val jda = handler.jda
-            val updateAction = jda.updateCommands()
-            val comNameMap = commandHandler.commandsByName
-
-            commandHandler.commands
-                .filter {
-                    it supports ECommandSource.COMMAND
-                            && it supports ChannelType.PRIVATE
-                            && it supports ChannelType.TEXT
-                }
-                .forEach {
-                    val commandData = CommandData(it.name.lowercase().shortenTo(32), it.description.shortenTo(100))
-
-                    it.parameters.forEach { param ->
-                        val optionType = when (param.type) {
-                            EParameterType.BOOLEAN   -> OptionType.BOOLEAN
-                            EParameterType.INTEGER   -> OptionType.INTEGER
-                            EParameterType.STRING    -> OptionType.STRING
-                            EParameterType.USER      -> OptionType.USER
-                            EParameterType.TIMESTAMP -> OptionType.STRING
-                            EParameterType.DATE      -> OptionType.STRING
-                            EParameterType.TIME      -> OptionType.STRING
-                        }
-                        val name = if (param.name.isEmpty()) "args" else param.name
-                        commandData.addOption(optionType, name.lowercase().shortenTo(32), param.description.shortenTo(100))
-                    }
-
-                    updateAction.addCommands(commandData)
-                }
-            updateAction.queue { commands ->
-                commands
-                    .mapNotNull { comNameMap[it.name]?.let { com -> it.idLong to com } }
-                    .forEach { commandMap[it.first] = it.second }
+        commandHandler.commands
+            .filter {
+                it supports ECommandSource.COMMAND
+                        && it supports ChannelType.PRIVATE
+                        && it supports ChannelType.TEXT
             }
+            .forEach {
+                logger().debug("Adding \"${it.name}\" as native Discord command")
+                val commandData = CommandData(it.name.lowercase().shortenTo(32), it.description.shortenTo(100))
+
+                it.parameters.forEach { param ->
+                    val optionType = when (param.type) {
+                        EParameterType.BOOLEAN   -> OptionType.BOOLEAN
+                        EParameterType.INTEGER   -> OptionType.INTEGER
+                        EParameterType.STRING    -> OptionType.STRING
+                        EParameterType.USER      -> OptionType.USER
+                        EParameterType.TIMESTAMP -> OptionType.STRING
+                        EParameterType.DATE      -> OptionType.STRING
+                        EParameterType.TIME      -> OptionType.STRING
+                    }
+                    val name = if (param.name.isEmpty()) "args" else param.name
+                    logger().trace("Adding `--$name` as parameter to ${it.name}")
+                    commandData.addOption(optionType, name.lowercase().shortenTo(32), param.description.shortenTo(100))
+                }
+
+                updateAction.addCommands(commandData)
+            }
+        updateAction.queue { commands ->
+            logger().info("Commands sucessfully updated at Discord")
+            commands
+                .mapNotNull { comNameMap[it.name]?.let { com -> it.idLong to com } }
+                .forEach { commandMap[it.first] = it.second }
         }
     }
+
 
     override fun onSlashCommand(event: SlashCommandEvent) {
         val guildId = if (event.isFromGuild) event.guild?.idLong ?: 0L else 0L
         val channelId = if (event.isFromGuild) event.channel.idLong else event.user.idLong
         val command = commandMap[event.commandIdLong]
+        logger().info("Processing native Discord command from guild $guildId, channel $channelId: ${event.commandPath}")
+        logger().debug("Associated native command with $command")
 
         if (command != null) {
             val parameters = command.parameters.map { it.copy() }
@@ -122,6 +119,7 @@ class SlashCommandListener(
                         ?: it.asTime()
                 }
             }
+            logger().trace("Loaded parameters: $parameters")
 
             val additionalVars: Variables = hashMapOf()
             additionalVars[FlunderKey.Flunder.Command.User.Name] = event.user.name
@@ -143,7 +141,7 @@ class SlashCommandListener(
                 )
                 event.reply("Befehl erfolgreich").setEphemeral(true).queue()
             } catch (ex: Exception) {
-                logger().error("Error when executing slash-command", ex)
+                logger().error("Error when executing native Discord command", ex)
                 event.reply("Ein Fehler ist aufgetreten").setEphemeral(true).queue()
             }
         }
